b22d1943426da8aa972e010846d3a5a139c60778
kooji <guikooji11@hotmail.com>
2026-05-07 11:49:17 -0300

    hasbulla

--- src/js/script.js
+++ src/js/script.js
@@ -11,7 +11,19 @@ window.onload = function(){
     },3000)
 }
 
+// menu hamburguer
 
+document.addEventListener('DOMContentLoaded',()=>{
+    const menuIcone= document.getElementById("menu-icone");
+    const navMenu = document.querySelector("nav");
+    if(menuIcone && navMenu){
+        menuIcone.onclick=()=>{
+            navMenu.classList.toggle("active");
+            menuIcone.classList.toggle("open");
+        }
+    }
+
+})
 
 function trocar(cor){
     document.body.style.background=cor;
